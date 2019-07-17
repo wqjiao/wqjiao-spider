883be899c1ed4e15eb0d6d2777a043a4585d109d
wqjiao <wqjiao@che300.com>
2019-07-17 18:32:26 +0800

    gengxin

--- apis/interview.js
+++ apis/interview.js
@@ -2,7 +2,7 @@
  * @Author: wqjiao
  * @Date: 2019-03-25 17:57:51
  * @Last Modified by: wqjiao
- * @Last Modified time: 2019-07-17 17:35:32
+ * @Last Modified time: 2019-07-17 18:24:33
  * @Description: interview
  */
 const router = require('koa-router')();
@@ -46,7 +46,6 @@ router.get('/interview/list', async (ctx, next) => {
 // 获取详情数据
 router.post('/interview/detail', async (ctx, next) => {
     const req = ctx.request.body;
-    console.log("**", req)
     const examples = await interviewList.find(req, { _id: 0 });
   
     ctx.status = 200;
@@ -59,6 +58,52 @@ router.post('/interview/detail', async (ctx, next) => {
           total: examples[0].data.length
         }
     }
+});
+
+// 更新数据库
+function onUpdate(query, item) {
+    return new Promise(async (resolve, reject) => {
+        await leetCodeSchema.updateOne(query, item)
+            .then((data) => {
+                console.log(`更新数据库成功 ${item.questionId}`);
+                resolve(data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
+// 提交答案
+router.post('/interview/answer', async (ctx, next) => {
+    const req = ctx.request.body;
+    let item;
+
+    await interviewList.findOne(req)
+        .then(async (res) => {
+            if (res) {
+                item = {
+                    ...res,
+                    data: [...res.data, {name: '111', content: req.content}]
+                }
+                await interviewList.updateOne(query, item)
+                    .then((data) => {
+                        resolve(data);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });;
+            }
+            ctx.status = 200;
+            ctx.body = {
+                code: 200,
+                msg: '提交成功',
+                data: {}
+            }
+        })
+        .catch((err) => {
+            reject(err);
+        });
 });
 
 module.exports = router;
